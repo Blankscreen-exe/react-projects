4008b410d19c70866a09a41df5532fa56665b14c
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2022-10-15 23:26:17 +0500

    add app file

---
+++ 03-airbnb_clone/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import 'fs';
+import './App.css';
+
+// components
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Card from './components/Card';
+
+// database
+import cardList from '../database/dataLoader';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <section className='card-container'>
+        {cardList.map((item) => 
+        <Card
+          key={item._id}
+          name={item.name} 
+          profile_url={item.profile_url}
+          title={item.title}
+          price={item.price}
+          rating={item.rating}
+          reviewCount={item.reviewCount}
+          location={item.location}
+          />)}
+        </section>
+    </div>
+  )
+}
+
+export default App
