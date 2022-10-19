52ac48da3c8264e01691c1c2d88e7bded35c7d46
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2022-10-20 00:26:02 +0500

    add navbar component

---
+++ 05-meme_generator/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function () {
+    return (
+        <header className="header">
+            <img 
+                src="./doge.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Doge Meme Generator</h2>
+            <h4 className="header--project">Make this doggo laugh!</h4>
+        </header>
+    );
+}
